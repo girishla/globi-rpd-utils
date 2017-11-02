edb866ddf87feb2a094e04373172512974a58822
Girish Lakshmanan <girish.lakshmanan@lexisnexis.co.uk>
2017-11-02 14:40:54 +0000

    minor refactor

--- src/main/java/com/globi/rpd/operator/XudmlUnmarshallingOperator.java
+++ src/main/java/com/globi/rpd/operator/XudmlUnmarshallingOperator.java
@@ -1,11 +1,13 @@
 package com.globi.rpd.operator;
 
 import com.globi.rpd.Operator;
+import com.globi.rpd.component.BusinessModel;
 import com.globi.rpd.component.PresentationCatalog;
 import com.globi.rpd.component.PresentationTable;
 import com.globi.rpd.xudml.ResourceFactory;
 import com.globi.rpd.xudml.XudmlMarshaller;
 
+import xudml.BusinessModelW;
 import xudml.PresentationCatalogW;
 import xudml.PresentationTableW;
 
@@ -16,33 +18,39 @@ public class XudmlUnmarshallingOperator implements Operator {
 
 		XudmlMarshaller<PresentationCatalogW> marshaller = new XudmlMarshaller<PresentationCatalogW>();
 
-		
 		presCatalog.setXudmlObject(marshaller.unmarshall(ResourceFactory.fromURL(presCatalog.getResourceUri())));
-		
-		presCatalog.getXudmlObject()//
-		.getRefTables()//
-		.getRefPresentationTable()//
-		.stream()//
-		.forEach(table -> {
-			presCatalog.getPresentationTables()
-					.add(new PresentationTable(table.getRefId()));
 
-		});
-		
+		presCatalog.getXudmlObject()//
+				.getRefTables()//
+				.getRefPresentationTable()//
+				.stream()//
+				.forEach(table -> {
+					presCatalog.getPresentationTables()
+							.add(new PresentationTable(table.getRefId()));
+
+				});
 
 		return presCatalog;
 	}
 
 	@Override
 	public PresentationTable operate(PresentationTable presTable) {
-	
-		XudmlMarshaller<PresentationTableW> marshaller = new XudmlMarshaller<PresentationTableW>();
 
+		XudmlMarshaller<PresentationTableW> marshaller = new XudmlMarshaller<PresentationTableW>();
 		presTable.setXudmlObject(marshaller.unmarshall(ResourceFactory.fromURL("file:" + presTable.getResourceUri())));
-		
-		
+
 		return presTable;
-		
+
+	}
+
+	@Override
+	public BusinessModel operate(BusinessModel model) {
+
+		XudmlMarshaller<BusinessModelW> marshaller = new XudmlMarshaller<BusinessModelW>();
+		model.setXudmlObject(marshaller.unmarshall(ResourceFactory.fromURL("file:" + model.getResourceUri())));
+
+		return model;
+
 	}
 
 }
