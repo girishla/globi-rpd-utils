acdfb0ab22db8e122c383c36dcee6da510d6f838
Girish Lakshmanan <girish.lakshmanan@lexisnexis.co.uk>
2017-11-14 15:37:37 +0000

    added traversal operations for physical objects

--- src/main/java/com/globi/rpd/traverser/DefaultTraverser.java
+++ src/main/java/com/globi/rpd/traverser/DefaultTraverser.java
@@ -2,14 +2,18 @@
 package com.globi.rpd.traverser;
 
 import com.globi.rpd.component.BusinessModel;
+import com.globi.rpd.component.ConnectionPool;
+import com.globi.rpd.component.Database;
 import com.globi.rpd.component.LogicalColumn;
 import com.globi.rpd.component.LogicalTable;
+import com.globi.rpd.component.PhysicalTable;
 import com.globi.rpd.component.PresentationCatalog;
 import com.globi.rpd.component.PresentationColumn;
 import com.globi.rpd.component.PresentationHierarchy;
 import com.globi.rpd.component.PresentationLevel;
 import com.globi.rpd.component.PresentationTable;
 import com.globi.rpd.component.RpdComponent;
+import com.globi.rpd.component.Schema;
 import com.globi.rpd.operator.Operator;
 
 
@@ -39,11 +43,9 @@ public class DefaultTraverser implements Traverser {
 
 	@Override
 	public void traverse(BusinessModel model, Operator<? extends RpdComponent> anOperator) {
-
 		for (LogicalTable table : model.getLogicalTables()) {
 			table.apply(anOperator);
 		}
-
 	}
 
 	@Override
@@ -61,5 +63,23 @@ public class DefaultTraverser implements Traverser {
 		}
 
 	}
+	
+	@Override
+	public void traverse(Database db, Operator<? extends RpdComponent> anOperator) {
+		for (ConnectionPool cp : db.getConnectionPools())
+			cp.apply(anOperator);
+
+		for (Schema schema : db.getSchemas())
+			schema.apply(anOperator);
+		
+	}
+	
+	@Override
+	public void traverse(Schema schema, Operator<? extends RpdComponent> anOperator) {
+		for (PhysicalTable table : schema.getPhysicalTables())
+			table.apply(anOperator);
+		
+	}
+	
 
 }
